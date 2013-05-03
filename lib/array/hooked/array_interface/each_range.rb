1b3b5ac23221aa88b1fc9a7af6adbec6a916bf7b
Asher Haig <asher@ridiculouspower.com>
2013-05-03 11:24:25 -0400

    made each_range and reverse_each_range only take start, end instead of either order

--- lib/array/hooked/array_interface/each_range.rb
+++ lib/array/hooked/array_interface/each_range.rb
@@ -9,26 +9,14 @@ module ::Array::Hooked::ArrayInterface::EachRange
   ###
   # Iterates a range in self between index_one and index_two from left to right.
   #
-  def each_range( index_one, index_two = -1 )
+  def each_range( range_start = 0, range_end = -1 )
 
     return to_enum unless block_given?
     
     element_count = nil
-    index_one = index_one < 0 ? ( element_count = size ) + index_one : index_one
-    index_two = index_two < 0 ? ( element_count || size ) + index_two : index_two
+    range_start = range_start < 0 ? ( element_count = size ) + range_start : range_start
+    range_end = range_end < 0 ? ( element_count || size ) + range_end : range_end
 
-    range_start = nil
-    range_end = nil
-    
-    # renumber local => parent in range
-    if index_one < index_two
-      range_start = index_one
-      range_end = index_two
-    else
-      range_start = index_two
-      range_end = index_one
-    end
-    
     # copy parent => local to local => parent as appropriate
     number_of_indexes_modified = range_end - range_start + 1
     number_of_indexes_modified.times do |this_time|
@@ -48,26 +36,14 @@ module ::Array::Hooked::ArrayInterface::EachRange
   ###
   # Iterates a range in self between index_one and index_two from right to left.
   #
-  def reverse_each_range( index_one = -1, index_two = 0 )
+  def reverse_each_range( range_start = -1, range_end = 0 )
 
     return to_enum unless block_given?
 
     element_count = nil
-    index_one = index_one < 0 ? ( element_count = size ) + index_one : index_one
-    index_two = index_two < 0 ? ( element_count || size ) + index_two : index_two
+    range_start = range_start < 0 ? ( element_count = size ) + range_start : range_start
+    range_end = range_end < 0 ? ( element_count || size ) + range_end : range_end
 
-    range_start = nil
-    range_end = nil
-    
-    # renumber local => parent in range
-    if index_one < index_two
-      range_start = index_two
-      range_end = index_one
-    else
-      range_start = index_one
-      range_end = index_two
-    end
-    
     # copy parent => local to local => parent as appropriate
     number_of_indexes_modified = range_start - range_end + 1
     number_of_indexes_modified.times do |this_time|
